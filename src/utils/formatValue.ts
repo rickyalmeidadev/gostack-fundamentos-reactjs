492560ead93670301cd1edcae65281223b6cf39e
Ricky Almeida <ricky.almeida@nave.rs>
2020-12-31 19:23:49 -0300

    feat: list transactions, show balance and upload csv

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,9 @@
+export const formatDate = (date: Date): string =>
+  Intl.DateTimeFormat('pt-BR').format(date);
+
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', { style: 'currency', currency: 'BRL' }).format(
+    value,
+  );
 
 export default formatValue;
